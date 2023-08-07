36fcbbd4134f16ffcda6fc3dfee2ed9bc0504a1c
Andriel Suarez <andriel.suarez@awsys-i.com>
2023-08-07 09:27:22 +0800

    Modification

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import ToDo from './components/todo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
+          To-do list
+        </div>
+        <div className='card-body'>
+          <table className='table table-hover'>
+            <thead>
+              <tr>
+                <th scope='col'>Task No.</th>
+                <th scope='col'>Task Desc.</th>
+                <th scope='col'>Assigned to</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>React.js</td>
+                <td>Andriel</td>
+              </tr>
+              <ToDo/>
+            </tbody>
+            <button className='btn btn-primary mt-5'>Add To-Do</button>
+
+          </table>
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/todo.js
@@ -0,0 +1,11 @@
+function ToDo(){
+    return(
+            <tr>
+                <th scope='row'>2</th>
+                <td>Java script</td>
+                <td>Felix</td>
+            </tr>
+    )
+}
+
+export default ToDo;
